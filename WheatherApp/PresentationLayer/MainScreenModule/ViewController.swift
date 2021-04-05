5e0f4c805c2591c99e6ee7efd93dc018a7e99770
Konstantin <konstantin.petrikevich@gmail.com>
2021-04-05 21:31:55 +0300

    CurrentLocation not working

--- WheatherApp/PresentationLayer/MainScreenModule/ViewController.swift
+++ WheatherApp/PresentationLayer/MainScreenModule/ViewController.swift
@@ -1,7 +1,14 @@
 import Foundation
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    // MARK: - Properties
+    var currentCoordinate: CLLocationCoordinate2D?
+    lazy var locationManager = CLLocationManager()
+    
+    
+    // MARK: - Views methods
     override func loadView() {
         let mainScreen = MainScreenView()
         self.view = mainScreen
@@ -10,16 +17,64 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        WeatherAPIManager.fetch(type: WeatherResponse.self, router: WeatherRouter.fetchWeatherOneCall(53.9006, 27.5590, "minutely,alerts", AppEnvironment.apiKey)) { result in
-            switch result {
-            case .failure(let error):
-                print("ERROR: \(error.localizedDescription)")
-            case .success(let response):
-                DispatchQueue.main.async {
-                    (self.view as? MainScreenView)?.updateView(with: response)
+        // Find current location
+        if locationManager.authorizationStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        if locationManager.authorizationStatus == .denied || locationManager.authorizationStatus == .restricted {
+            showLocationDeniedAlert()
+            return
+        }
+    
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+            locationManager.startUpdatingLocation()
+        } else {
+            print("Off")
+        }
+        
+        
+        if let coordinate = currentCoordinate {
+            // Fetch data and update MainScreen
+            WeatherAPIManager.fetch(type: WeatherResponse.self, router: WeatherRouter.fetchWeatherOneCall(coordinate.latitude, coordinate.longitude, "minutely,alerts", AppEnvironment.apiKey)) { result in
+                switch result {
+                case .failure(let error):
+                    print("ERROR: \(error.localizedDescription)")
+                case .success(let response):
+                    DispatchQueue.main.async {
+                        (self.view as? MainScreenView)?.updateView(with: response)
+                    }
                 }
             }
-        }  
+        }
+        locationManager.stopUpdatingLocation()
+    }
+    
+    // MARK: - Private methods
+    private func showLocationDeniedAlert(){
+        let alert = UIAlertController(title: "Location Services Disabled", message: "Enable location services for 'Map-Tag' in Settings", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+// MARK: - Extensions
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currentLocation = locations[0]
+        
+        print(currentLocation.coordinate.latitude)
+        print(currentLocation.coordinate.longitude)
+        currentCoordinate = currentLocation.coordinate
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("ERROR: \(error.localizedDescription)")
     }
 }
